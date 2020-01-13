ebe69f596d6fe4a736961a1fb5481fa6ec995593
moringaschool-Winnie <mwikaliwinnie303@gmail.com>
2020-01-13 08:32:41 +0300

    Remove errors that arise when build

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Quote } from './../quote';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
